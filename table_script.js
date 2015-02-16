01f07e92c4b9f1f1b099f8804bb4e15d2d0faccc
Admin <Admin@codeandsuch.com>
2015-02-16 11:35:32 -0600

    dont know

--- table_script.js
+++ table_script.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
-   $('div').mouseenter(function() 
+   $('.boxdiv').mouseenter(function() 
    {
        $(this).animate({
            height: '+=10px'
        });
    });
    
-   $('div').mouseleave(function() {
+   $('.boxdiv').mouseleave(function() {
        $(this).animate({
            height: '-=10px'
        });
    });
    
+// ********** Custom for table_test.html *********  
+
+// When you hover over a TH cell, apply the highlight class to it
    $('th').hover(function(){
     $(this).toggleClass('highlight');
 });
 
+// When you hover over a TD cell, apply the highlight class to it
    $('td').hover(function(){
     $(this).toggleClass('highlight');
 });
+
+// When you hover over the table2 class, apply the border class to it
+			// and apply the border class to all TD cells in table2 class
+	$('.table2').hover(function(){
+    $(this).toggleClass('border');
+	$('table2 > td').toggleClass('border');
+});
    
-   $('div').click(function() {
-       $(this).toggle(1000);
-   }); 
 });
